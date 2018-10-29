4460e96994f4c5ef50e4d0c7137cb499a9b7b77f
见贤不思齐 <1151143436@qq.com>
2018-10-29 18:01:03 +0800

    基础环境搭建

---
+++ hello-provider/src/main/java/com/suns/cloud/helloprovider/HelloProviderApplication.java
@@ -0,0 +1,12 @@
+package com.suns.cloud.helloprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloProviderApplication.class, args);
+    }
+}
